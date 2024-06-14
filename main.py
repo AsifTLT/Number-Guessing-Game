c3d2add018ba7da2d27ea6a030c182bd350e5b05
Mohammad Asif <akonasif321@gmail.com>
2024-06-14 14:44:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,28 @@ else:
     print("print type number next time.")
     quit()    
 random_number = random.randint(0, top_of_range)
-# print(random_number)
+guesses = 0
+
 while True:
+    guesses += 1
+    user_guess = input("Make a Guess: ")
+    if user_guess.isdigit():
     
+        user_guess = int(user_guess)
+    else:
+        print("print type number next time.")
+        continue
+    print("After Continue")
     
-    break
-    
+    if user_guess == random_number:
+        print("you got it!")
+        break  
+
+    elif user_guess > random_number:
+        print("You were above the number")
+    else:
+        print("You were below the number")
+                
+
+print("You got it in",  guesses, "guesses")    
 
